58f732c0981a80465bfde0deb11736735ebf3b8c
eneguE <bogdevich96@hotmail.com>
2018-03-13 15:57:31 +0300

    Initial commit

---
+++ src/main/java/com/bogdevich/bulbs/BulbsApplication.java
@@ -0,0 +1,12 @@
+package com.bogdevich.bulbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulbsApplication.class, args);
+	}
+}
